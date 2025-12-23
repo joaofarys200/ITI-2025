6de2299e33c66b57194a6e5afe5f3442d08e13e7
Joao Faria <joaorpg2015@gmail.com>
2025-12-23 23:32:34 +0000

    added new files

--- auth.py
+++ auth.py
@@ -1,19 +1,31 @@
-from flask import Flask, request, jsonify, send_file,render_template,url_for,redirect
+from flask import Flask, request, jsonify, send_file,render_template,url_for,redirect, Response
 import os
+import socket
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 #observacoes feitas,apenas funciona por causa do html,sem ele nao consigo redirecionar os url,logo tenho que testar no postman a api.
 app = Flask(__name__)
 
-BASE_DIR = "storage"
-os.makedirs(BASE_DIR, exist_ok=True)
+# Dentro do container, a pasta partilhada é montada em /app/storage (ver docker-compose.yml)
+BASE_DIR = "/app/storage"
 app.config["BASE_DIR"] = BASE_DIR
+#coisas para o prometheus
+REQUEST_COUNT = Counter("app_requests_total", "Total de pedidos HTTP", ["endpoint", "hostname"])
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.labels(request.path, socket.gethostname()).inc()
+
 
 @app.route("/")
 def home():
+    print(f"Home handled by {socket.gethostname()}")
     files=os.listdir(BASE_DIR)
     return render_template("index.html", files=files)
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
+    print(f"Upload handled by {socket.gethostname()}")
     if 'file' not in request.files:
         return "No file part", 400
     file = request.files['file']
@@ -34,7 +46,15 @@ def delete_file(filename):
         os.remove(file_path)
     return redirect(url_for('home'))
 
+
+socket.gethostname()
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 #add to git
